e6a1788c7698c253766d731d1f2810eb99d349f4
Freilla <freillamae13@gmail.com>
2017-07-19 19:30:16 +0800

    Add markup

---
+++ index.php
@@ -0,0 +1,64 @@
+
+	<section class="intro-content">
+		<?php include('includes/heading.php'); ?>
+
+		<div class="content house">
+				<div class="mobile">
+					<img src="images/house.png">
+				</div>
+				<div class="left-panel">
+					<h1>Is your home making you sick?</h1>
+					<p>Hayward Score will help you find out. Your answers to the survey will help us understand the important characteristics of your home and how you live in your home. From there we will generate a detailed report that will help you understand your indoor air quality and how you can improve your air and your health</p>
+					<a href="#" class="btn btn-started">Let's Get Started!</a>
+				</div>
+				<div class="right-panel desktop">
+					<img src="images/house.png">
+				</div>
+				<div class="clearfix"></div>
+		</div>
+
+		<div class="content air-quality">
+			<div class="mobile">
+				<img src="images/clean.png">
+			</div>
+			<div class="right-panel">
+				<h2>Indoor Air Quality</h2>
+				<p>It turns out air is a lot like God. I think that I shall never see. A poem lovely as a tree. A tree whose hungry mouth is prest. Against the earth’s sweet flowing breast; A tree that looks at God all day; And lifts her leafy arms to pray;</p>
+				<div class="statistics">
+					<div class="numbers">
+						<span class="green">10%</span><p>of homeowners have experienced leak</p>
+					</div>
+					<div class="numbers">
+						<span class="green">47%</span><p>of homeowners have found score beneficial</p>
+					</div>
+				</div>
+				
+			</div>
+			<div class="left-panel desktop">
+				<img src="images/clean.png">
+			</div>
+			<div class="clearfix"></div>
+		</div>
+
+		<div class="content score">
+			<div class="mobile">
+				<img src="images/score.png">
+			</div>
+			<div class="left-panel">
+				<h2>Why Hayward Score</h2>
+				<p>If you think your home may be causing you harm. Some for the glories of this world; and some Sigh for The Prophet’s Paradise to come; Ah, take the cash and let the credit go, Nor heed the rumble of a distant drum</p>
+				<div class="subgraph">
+					<h3>After Score</h3>
+					<p>We will send you an action plan for the most vulnerable parts of your home</p>
+				</div>
+			</div>
+			<div class="right-panel desktop">
+				<img src="images/score.png">
+			</div>
+			<div class="clearfix"></div>
+		</div>
+
+		<div class="content get-score">
+			<a href="#" class="btn btn-score">Get Your Score!</a>
+		</div>
+	</section>
