ac8c1521215adf65ab4a99255e67f5100d0a6b38
Freilla <freillamae13@gmail.com>
2017-07-20 10:57:31 +0800

    update 1.0

--- index.php
+++ index.php
@@ -1,64 +1,30 @@
+<!doctype html>
 
-	<section class="intro-content">
-		<?php include('includes/heading.php'); ?>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
 
-		<div class="content house">
-				<div class="mobile">
-					<img src="images/house.png">
-				</div>
-				<div class="left-panel">
-					<h1>Is your home making you sick?</h1>
-					<p>Hayward Score will help you find out. Your answers to the survey will help us understand the important characteristics of your home and how you live in your home. From there we will generate a detailed report that will help you understand your indoor air quality and how you can improve your air and your health</p>
-					<a href="#" class="btn btn-started">Let's Get Started!</a>
-				</div>
-				<div class="right-panel desktop">
-					<img src="images/house.png">
-				</div>
-				<div class="clearfix"></div>
-		</div>
+	<title>Devcon Summit CDO 2017</title>
 
-		<div class="content air-quality">
-			<div class="mobile">
-				<img src="images/clean.png">
-			</div>
-			<div class="right-panel">
-				<h2>Indoor Air Quality</h2>
-				<p>It turns out air is a lot like God. I think that I shall never see. A poem lovely as a tree. A tree whose hungry mouth is prest. Against the earth’s sweet flowing breast; A tree that looks at God all day; And lifts her leafy arms to pray;</p>
-				<div class="statistics">
-					<div class="numbers">
-						<span class="green">10%</span><p>of homeowners have experienced leak</p>
-					</div>
-					<div class="numbers">
-						<span class="green">47%</span><p>of homeowners have found score beneficial</p>
-					</div>
-				</div>
-				
-			</div>
-			<div class="left-panel desktop">
-				<img src="images/clean.png">
-			</div>
-			<div class="clearfix"></div>
-		</div>
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/bootstrap-theme.min.css">
 
-		<div class="content score">
-			<div class="mobile">
-				<img src="images/score.png">
-			</div>
-			<div class="left-panel">
-				<h2>Why Hayward Score</h2>
-				<p>If you think your home may be causing you harm. Some for the glories of this world; and some Sigh for The Prophet’s Paradise to come; Ah, take the cash and let the credit go, Nor heed the rumble of a distant drum</p>
-				<div class="subgraph">
-					<h3>After Score</h3>
-					<p>We will send you an action plan for the most vulnerable parts of your home</p>
+</head>
+
+<body>
+	<section class="main">
+		<div class="container">
+			<div class="right-content">
+				<div class="logo-container">
+					<img src="images/logo.png">
+					<h1>Devcon Summit: CDO Edition</h1>
+					<p>The Biggest Developer Conference in the Philippines Goes to Cagayan de Oro City!</p>
 				</div>
 			</div>
-			<div class="right-panel desktop">
-				<img src="images/score.png">
-			</div>
-			<div class="clearfix"></div>
-		</div>
-
-		<div class="content get-score">
-			<a href="#" class="btn btn-score">Get Your Score!</a>
 		</div>
 	</section>
+	<script src="js/jquery-2.2.4.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
