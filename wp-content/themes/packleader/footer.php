5dc1119b5ff14db86614941108c9c36a1c9ea934
Freilla <freillamae13@gmail.com>
2017-07-24 14:04:23 +0800

    Markup only.

--- wp-content/themes/packleader/footer.php
+++
@@ -1,5 +0,0 @@
-	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/bootstrap.min.js"></script>
-	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/surprise.min.js"></script>
-	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/main.js"></script>
-</body>
-</html>
--- wp-content/themes/packleader/sidebar.php
+++
@@ -1,9 +0,0 @@
-<aside>			
-
-		<?php if ( !function_exists('dynamic_sidebar')
-			|| !dynamic_sidebar('Sidebar') ) : ?>
-	<?php endif; ?>  
-	
-</aside>
-
-<div class="clear"></div>	
